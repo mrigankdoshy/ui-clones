400763075a1a4554a399bf0a164a1937819bfa88
mrigankdoshy <mrigankdoshy@gmail.com>
2021-12-31 15:31:42 -0500

    Init Netflix Project

---
+++ netflix/lib/screens/home/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      // bottomNavigationBar: BottomNavigation(),
+      // body: Body(),
+    );
+  }
+}
