e9ef3625e39e7583144dec6b156d9623030dbfdc
mrigankdoshy <mrigankdoshy@gmail.com>
2022-01-12 19:49:12 -0500

    Complete snap

--- snapchat/lib/utils/snap_icon.dart
+++ snapchat/lib/utils/snap_icon.dart
@@ -1,16 +1,13 @@
-const sentWithoutAudio =
-    "assets/chat/sent_without_audio.png"; // Change to delivered
+const sentWithoutAudio = "assets/chat/sent_without_audio.png";
 const sentWithAudio = "assets/chat/sent_with_audio.png";
 const sentChat = "assets/chat/sent_chat.png";
 const openedWithoutAudio = "assets/chat/opened_without_audio.png";
 const openedWithAudio = "assets/chat/opened_with_audio.png";
 const openedChat = "assets/chat/opened_chat.png";
-const receivedWithoutAudio =
-    "assets/chat/received_without_audio.png"; //Change to New Snap
+const receivedWithoutAudio = "assets/chat/received_without_audio.png";
 const receivedWithAudio = "assets/chat/received_with_audio.png";
 const receivedChat = "assets/chat/received_chat.png";
-const viewedWithoutAudio =
-    "assets/chat/viewed_without_audio.png"; // Change to received
+const viewedWithoutAudio = "assets/chat/viewed_without_audio.png";
 const viewedWithAudio = "assets/chat/viewed_with_audio.png";
 const viewedChat = "assets/chat/viewed_chat.png";
 const screenshotWithoutAudio = "assets/chat/screenshot_without_audio.png";
--- snapchat/lib/widgets/snap.dart
+++ snapchat/lib/widgets/snap.dart
@@ -1,3 +1,4 @@
+import 'package:emojis/emojis.dart';
 import 'package:flutter/material.dart';
 import 'package:snapchat/widgets/bitmoji.dart';
 
@@ -5,11 +6,20 @@ class Snap extends StatelessWidget {
   final String bitmoji;
   final String name;
   final String snapIcon;
+  final String snapType;
+  final String time;
+  final String streak;
+  final String emoji;
+
   const Snap({
     Key? key,
     required this.bitmoji,
     required this.name,
     required this.snapIcon,
+    required this.snapType,
+    required this.time,
+    required this.streak,
+    required this.emoji,
   }) : super(key: key);
 
   @override
@@ -18,10 +28,10 @@ class Snap extends StatelessWidget {
       padding: const EdgeInsets.only(left: 8.0, right: 8.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Bitmoji(image: bitmoji),
               Column(
@@ -40,7 +50,6 @@ class Snap extends StatelessWidget {
                   const SizedBox(height: 8),
                   Wrap(
                     spacing: 8,
-                    // mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image.asset(
                         snapIcon,
@@ -48,7 +57,7 @@ class Snap extends StatelessWidget {
                         width: 12,
                       ),
                       Text(
-                        "Delivered",
+                        snapType,
                         style: TextStyle(
                           fontFamily: "Metropolis",
                           fontWeight: FontWeight.w500,
@@ -69,7 +78,7 @@ class Snap extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        "2h",
+                        time,
                         style: TextStyle(
                           fontFamily: "Metropolis",
                           fontWeight: FontWeight.w500,
@@ -90,10 +99,10 @@ class Snap extends StatelessWidget {
                         ),
                       ),
                       Container(
-                        transform: Matrix4.translationValues(0.0, -4.5, 0.0),
-                        child: const Text(
-                          "436🔥",
-                          style: TextStyle(
+                        transform: Matrix4.translationValues(0.0, -5.0, 0.0),
+                        child: Text(
+                          "$streak${Emojis.fire}",
+                          style: const TextStyle(
                             fontFamily: "Metropolis",
                             fontWeight: FontWeight.w500,
                             fontSize: 12,
@@ -102,17 +111,33 @@ class Snap extends StatelessWidget {
                         ),
                       ),
                     ],
-                  )
+                  ),
                 ],
-              )
+              ),
             ],
           ),
-          Row(
-            children: const [
-              // Emoji
-              // Camera
-            ],
-          )
+          Padding(
+            padding: const EdgeInsets.only(top: 28.0),
+            child: Wrap(
+              spacing: 24.0,
+              children: [
+                Container(
+                  transform: Matrix4.translationValues(0.0, -4.0, 0.0),
+                  child: Text(
+                    emoji,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.photo_camera_outlined,
+                  color: Colors.white.withOpacity(0.7),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
